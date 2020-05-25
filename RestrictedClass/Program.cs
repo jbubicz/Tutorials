b927ad171e7c997cdd2f1d8b4ecdf5cdc7f3e120
jacek bubicz <jbubicz@gmail.com>
2020-05-25 16:00:39 +0200

    Add project files.

---
+++ RestrictedClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RestrictedClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Program();
+            }
+        }
+
+        private static int instanceCount { get; set; }
+
+        public Program()
+        {
+            if (instanceCount == 3)
+            {
+                throw new InvalidOperationException("oNLY 3 INSTANCES ALLOWED");
+            }
+            instanceCount ++;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Singleton
+{
+    interface Interface1
+    {
+        void TestInterface();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void TestStaticAndSingleton(SingletonClass singleton)
+        {
+
+        }
+    }
+
+    public static class StaticClass // : Interface1  -> error
+    {
+    }
+
+    public sealed class SingletonClass : Interface1
+    {
+        public void TestInterface()
+        {
+            throw new NotImplementedException();
+        }
+
+        private SingletonClass()
+        {
+
+        }
+
+        private static volatile SingletonClass instance;
+        private static object syncRoot = new Object();
+        private static SingletonClass Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new SingletonClass();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
