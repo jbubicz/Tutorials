272c8a96fd4e46c5f455877eb89c5d9bfb9ac80a
jacek bubicz <jbubicz@gmail.com>
2020-06-02 19:09:38 +0200

    20200602

---
+++ AppExtMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using ExtMethods;
+using System;
+using System.Reflection.Metadata;
+
+namespace AppExtMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtensionClass obj = new ExtensionClass();
+            int result = obj.MultiplicationNumber(20, 30);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            int Number = 20;
+            Console.WriteLine(Number.CheckOddEven());
+            Console.ReadLine();
+        }
+    }
+
+    public static class NumberCheckOddEven
+    {
+        public static string CheckOddEven(this Int32 Number)
+        {
+            if (Number % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+    }
+    public static class ExtendClass
+    {
+        public static int MultiplicationNumber(this ExtensionClass multiplication, int Number1, int Number2)
+        {
+            return Number1 * Number2;
+        }
+    }
+}
---
+++ ExtMethods/ExtensionClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtMethods
+{
+    public class ExtensionClass
+    {
+        public int AddNumber (int Number1, int Number2)
+        {
+            return Number1 + Number2; 
+        }
+
+        public int SubNumber(int Number1 , int Number2)
+        {
+            return Number1 - Number2;
+        }
+    }
+}
